e05ba2eefc0a88720e44c67d0c13ffdc5494271a
robbliebob <robbiedehaan@gmail.com>
2025-07-20 22:52:31 -0400

    Deleted unnecessary files and removed unneeded comments

--- triggers/report_absence_trigger.ts
+++ triggers/report_absence_trigger.ts
@@ -8,19 +8,16 @@ import ReportAbsenceWorkflow from "../workflows/report_absence.ts";
  * such as a user pressing a button or when a specific event occurs.
  * https://api.slack.com/automation/triggers
  */
+
 const reportAbsenceTrigger: Trigger<typeof ReportAbsenceWorkflow.definition> = {
   type: TriggerTypes.Shortcut,
   name: "Report Absence",
-  description: "Report a student or mentor absence",
+  description: "Report a student absence",
   workflow: `#/workflows/${ReportAbsenceWorkflow.definition.callback_id}`,
   inputs: {
     interactivity: {
       value: TriggerContextData.Shortcut.interactivity,
     },
-    // If your workflow requires googleAccessTokenId as input, add it here
-    // googleAccessTokenId: {
-    //   value: { credential_source: "DEVELOPER" },
-    // },
   },
 };
 
--- workflows/report_absence.ts
+++ workflows/report_absence.ts
@@ -1,29 +1,3 @@
-/* // FILE: report_absence_workflow.ts
-import { DefineWorkflow, Schema } from "deno-slack-sdk/mod.ts";
-import { ReportAbsenceFunctionDefinition } from "../functions/report_absence.ts";
-
-const ReportAbsenceWorkflow = DefineWorkflow({
-  callback_id: "report_absence_workflow",
-  title: "Report Absence",
-  description: "Prompt user to report absence and choose between recurring or one-time",
-  input_parameters: {
-    properties: {
-      interactivity: { type: Schema.slack.types.interactivity },
-      // Optionally, add googleAccessTokenId if you want to pass it in
-      // googleAccessTokenId: { type: Schema.slack.types.oauth2, oauth2_provider_key: "google" },
-    },
-    required: ["interactivity"],
-  },
-});
-
-// Add your custom function as a step
-ReportAbsenceWorkflow.addStep(ReportAbsenceFunctionDefinition, {
-  interactivity: ReportAbsenceWorkflow.inputs.interactivity,
-  googleAccessTokenId: { credential_source: "DEVELOPER" }, // or use workflow input if you add it above
-});
-
-export default ReportAbsenceWorkflow; */
-
 import { DefineWorkflow, Schema } from "deno-slack-sdk/mod.ts";
 import { ReportAbsenceFunctionDefinition } from "../functions/report_absence.ts";
 
